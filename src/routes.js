2072a6a49f8ff7251dcdf0697d4abc46cf559385
m7Aei <mateuspm2015@hotmail.com>
2019-08-08 14:59:57 -0300

    Criando todas as rotas da aplicação

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,19 @@
 import { Router } from 'express'
 
+import AlunoController from './controllers/AlunoController'
+import EscolaController from './controllers/EscolaController'
+
 const routes = Router()
+
+// Student CRUD routes
+routes.get('/alunos', AlunoController.index)
+routes.get('/alunos/:id', AlunoController.show)
+routes.post('/alunos', AlunoController.store)
+routes.put('/alunos/:id', AlunoController.update)
+routes.delete('/alunos/:id', AlunoController.destroy)
+
+// School routes
+routes.get('/escola', EscolaController.index)
+routes.post('/escola', EscolaController.store)
 
 export default routes
