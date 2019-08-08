96e77a674155193cf2ee5087ef594622ef2d6a9a
m7Aei <mateuspm2015@hotmail.com>
2019-08-08 14:33:07 -0300

    Criando o model da escola

---
+++ src/models/Escola.ts
@@ -0,0 +1,20 @@
+import { Schema, model, Document } from 'mongoose'
+
+interface EscolaInterface extends Document {
+  name: string,
+  alunos: Schema.Types.ObjectId
+}
+
+const EscolaSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  alunos: {
+    type: Schema.Types.ObjectId,
+    ref: 'Aluno',
+    required: true
+  }
+})
+
+export default model<EscolaInterface>('Escola', EscolaSchema)
